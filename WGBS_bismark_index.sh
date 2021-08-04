4f34a32aea5ad44283fff11e8f2afc54442026fd
XuYingJie <75149806+XuYingJie-z@users.noreply.github.com>
2021-08-04 21:17:43 +0800

    Update WGBS_bismark_index.sh

--- WGBS_bismark_index.sh
+++ WGBS_bismark_index.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 
+#  /public1/home/sc60357/reference/lambda/ 这个路径下面就是一个fasta文件
 bismark_genome_preparation --path_to_aligner /public1/home/sc60357/miniconda3/envs/python3/bin/ --bowtie2   --verbose  /public1/home/sc60357/reference/lambda/
